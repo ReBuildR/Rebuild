5a2f6726c96c92520b4a2b195dc53beba3f56d24
OzBirdog <ozbirdett@gmail.com>
2024-10-26 18:51:07 -0500

    moved chatbot

--- rebuild-app/src/App.jsx
+++ rebuild-app/src/App.jsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/Navbar'
 import { Home } from './components/pages/Home'
 import { Ideas } from './components/pages/Ideas'
 import { Submit } from './components/pages/Submit'
+import { Chatbot } from './components/pages/Chatbot'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/ideas" element={<Ideas />} />
           <Route path="/submit" element={<Submit />} />
+          <Route path="/answer" element={<Chatbot />} />
         </Routes>
     </div>
   )
