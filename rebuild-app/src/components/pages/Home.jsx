c5ed9403b2fda547defa7026c9a6638036db6cc0
OzBirdog <ozbirdett@gmail.com>
2024-10-26 16:27:10 -0500

    font

--- rebuild-app/src/components/pages/Home.jsx
+++ rebuild-app/src/components/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Button } from './Button'
 import './Home.css'
 
 export const Home = () => {
   return (
     <div className='home-container'>
         <div className="home-box">
-
+            <div className="buttons">
+                <Button text= "Rebuild" className="button-rebuild" link="/resume"/>
+                <Button text="Reuse" className="button-reuse" link="/projects"/>
+                <Button text= "Recycle" className="button-recycle" link="/resume"/>
+            </div>
         </div>
     </div>
   )
